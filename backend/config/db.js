824ff937b588cc0eeb1f41d9f81973cfd97b25f4
Prajwal J S <prajwaljs95@gmail.com>
2026-02-14 16:31:56 +0530

    Railway database connected

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,40 @@
-const mysql = require("mysql2");
-require("dotenv").config();
+// const mysql = require("mysql2");
+// require("dotenv").config();
 
-const db = mysql.createConnection({
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.error("❌ Database Connection Failed:", err.message);
+//   } else {
+//     console.log("✅ MySQL Connected Successfully");
+//   }
+// });
+
+// module.exports = db;
+
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
-db.connect((err) => {
+connection.connect((err) => {
   if (err) {
-    console.error("❌ Database Connection Failed:", err.message);
+    console.error("DB ERROR:", err);
   } else {
-    console.log("✅ MySQL Connected Successfully");
+    console.log("✅ Railway MySQL Connected");
   }
 });
 
-module.exports = db;
+module.exports = connection;
