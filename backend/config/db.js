dd817c412622e2d2953d84d5ab46060166710018
Prajwal J S <prajwaljs95@gmail.com>
2026-02-17 16:06:08 +0530

    Backend updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,13 @@ require("dotenv").config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT, // IMPORTANT for Railway
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD, // FIXED name
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false, // Required for Railway external connection
+  },
 });
 
 db.connect((err) => {
